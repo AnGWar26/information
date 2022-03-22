101693d6ec053b714f548454ecd92ea4acd60e1e
Andrew Warfield <andrew.warfield@protonmail.com>
2022-03-21 21:30:09 -0500

    init

---
+++ crawler.py
@@ -0,0 +1,7 @@
+import bs4
+import queue
+import util
+from node import Node
+
+def crawl(starting_url, num_pages_crawl):
+    
---
+++ node.py
@@ -0,0 +1,7 @@
+# Node data structure for holding necesary information
+
+class Node():
+
+    def __init__(self, url):
+        self.url = url
+        self.children = []
